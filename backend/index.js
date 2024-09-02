b936af2ff9b845ef94c30a23bd7924fea703b0fa
Jerome072902 <141805375+Jerome072902@users.noreply.github.com>
2024-09-02 09:40:12 +0800

    Add meeting backend

--- backend/index.js
+++ backend/index.js
@@ -110,3 +110,56 @@ app.post("/register", async (req, res) => {
     res.status(500).send("An error occurred while creating the user.");
   }
 });
+
+//Minutes of Meeting Logic
+
+//Add Meeting
+app.post("/minofmeet", async (req, res) => {
+  const {
+    title,
+    venue,
+    date,
+    startTime,
+    endTime,
+    duration,
+    attendees,
+    chairedBy,
+    presents,
+    absents,
+    highlights,
+  } = req.body;
+
+  const query = `
+    INSERT INTO minofmeet (
+      title, venue, date, startTime, endTime, duration, attendees,
+      chairedBy, presents, absents, highlights
+    ) VALUES (
+      @title, @venue, @date, @startTime, @endTime, @duration, @attendees,
+      @chairedBy, @presents, @absents, @highlights
+    )
+  `;
+
+  try {
+    const pool = await sql.connect();
+    await pool
+      .request()
+      .input("title", sql.VarChar, title)
+      .input("venue", sql.VarChar, venue)
+      .input("date", sql.VarChar, date)
+      .input("startTime", sql.VarChar, startTime)
+      .input("endTime", sql.VarChar, endTime)
+      .input("duration", sql.VarChar, duration)
+      .input("attendees", sql.NVarChar, JSON.stringify(attendees))
+      .input("chairedBy", sql.VarChar, chairedBy)
+      .input("presents", sql.NVarChar, JSON.stringify(presents))
+      .input("absents", sql.NVarChar, JSON.stringify(absents))
+      .input("highlights", sql.NVarChar, JSON.stringify(highlights))
+      .query(query);
+
+    console.log("Meeting has been created successfully");
+    res.json("Meeting has been created successfully");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
